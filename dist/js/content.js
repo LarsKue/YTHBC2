465b29ebc62ddf98ccb3737133b7a26881e0129f
Lars Kue <lars@kuehmichel.de>
2018-12-22 19:19:21 +0100

    Changed functionality from pasting an image atop the button to changing the actual button, which makes a lot more sense and is prettier.

--- dist/js/content.js
+++ dist/js/content.js
@@ -2,36 +2,15 @@ var isFullScreen = !!document.fullscreen;
 var buttonID = "NewHomeButton";
 var buttonURL = chrome.extension.getURL("assets/button.png");
 var vert_scroll = !!(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
-function showImage(b) {
-    document.getElementById(buttonID).style.visibility = b ? "visible" : "hidden";
-}
-function onFullScreenChange() {
-    isFullScreen = !isFullScreen;
-    console.log("You are now " + (isFullScreen ? "" : "not ") + "in FullScreen mode.");
-    showImage(!isFullScreen);
-}
-function onScroll() {
-    vert_scroll = !!(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
-    if (isFullScreen) {
-        showImage(vert_scroll);
-    }
-}
-function createButton() {
-    var linkItem = document.createElement("a");
-    linkItem.id = buttonID;
-    linkItem.href = "https://www.youtube.com/";
-    linkItem.innerHTML = "<img src=" + buttonURL + " title='YouTube Home'>";
-    linkItem.setAttribute("style", "position:fixed;" +
-        "top:15px;" +
-        "left:70px;" +
-        "z-index:3000;");
-    document.getElementsByTagName("body")[0].appendChild(linkItem);
+function execute() {
+    document.querySelectorAll("a[title='YouTube Home'").forEach(function (linkItem) {
+        linkItem.href = "https://www.youtube.com/";
+        linkItem.innerHTML = "<img src = " + buttonURL + ">";
+    });
 }
 chrome.runtime.sendMessage({ msg: "getEnabled" }, function (response) {
     if (response.enabled) {
-        createButton();
-        document.addEventListener("fullscreenchange", onFullScreenChange);
-        window.addEventListener("scroll", onScroll);
+        execute();
     }
     return true;
 });
--- src/content.ts
+++ src/content.ts
@@ -9,51 +9,20 @@ let buttonURL : string = chrome.extension.getURL("assets/button.png");
 // covers whether the user has scrolled down on all browsers
 let vert_scroll : boolean = !!(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
 
-function showImage(b : boolean) : void {
-    document.getElementById(buttonID).style.visibility = b ? "visible" : "hidden";
+function execute() : void {
+
+    // select all items from the document that have the title "YouTube Home" and give them the
+    // proper link and a regular YouTube Home image
+    document.querySelectorAll("a[title='YouTube Home'").forEach(function(linkItem : any) {
+        linkItem.href = "https://www.youtube.com/";
+        linkItem.innerHTML = `<img src = ${buttonURL}>`;
+    });
 }
 
-// called every time the user changes fullscreen mode on youtube
-function onFullScreenChange() : void {
-    isFullScreen = !isFullScreen;
-    console.log(`You are now ${isFullScreen ? "" : "not "}in FullScreen mode.`);
-
-    // hide the image when the user goes fullscreen
-    showImage(!isFullScreen);
-}
-
-function onScroll() : void {
-    // update the current scroll position
-    vert_scroll = !!(window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
-    // as of 22/12/18, YouTube will show the home button when in fullscreen, when the user scrolls down to the comments
-    if (isFullScreen) {
-        showImage(vert_scroll);
-    }
-}
-
-function createButton() : void {
-    const linkItem= document.createElement("a");
-    linkItem.id = buttonID;
-    linkItem.href = "https://www.youtube.com/";
-    linkItem.innerHTML = `<img src=${buttonURL} title='YouTube Home'>`;
-    linkItem.setAttribute("style",
-        "position:fixed;" +
-        "top:15px;" +
-        "left:70px;" +
-        "z-index:3000;");
-
-    document.getElementsByTagName("body")[0].appendChild(linkItem);
-}
-
-// check if the extension is enabled before loading the image
 chrome.runtime.sendMessage({msg: "getEnabled"}, function(response) {
-    // sending messages is asynchronous, so you can't really do this much differently without using storage api
-    if (response.enabled) {
-        createButton();
-        document.addEventListener("fullscreenchange", onFullScreenChange);
-        window.addEventListener("scroll", onScroll);
+    if(response.enabled) {
+        execute();
     }
-    // maybe need this here to prevent some asynchronosity?
     return true;
 });
 
